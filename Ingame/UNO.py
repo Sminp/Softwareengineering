a5dec64dac85e7a4a8c9bfa72399751c8fb9ac4c
Nayeon Kim <kny020716@naver.com>
2023-03-23 14:01:18 +0900

    김나연 일시정지, 설정화면 구현

--- Ingame/UNO.py
+++ Ingame/UNO.py
@@ -2,14 +2,9 @@ import pygame
 import sys
 # from game_functions import *
 
+
 pygame.init()
 
-# 색상
-black = (0,0,0)
-white = (255,255,255)
-gray = (200,200,200)
-red = (200,0,0)
-green = (0,80,0)
 
 # 상수
 # 카드 리스트 이미지 넣고 인덱스로 접근 (안은 튜플 앞, 뒤)
@@ -23,10 +18,13 @@ CARD_WIDTH = 410 / 5
 
 # 그리고 좌표도 화면 크기 조정 때문에 다 변수로 만들어야 될 것 같아.
 
-# 색깔
+# 색상
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 YELLOW = (255, 255, 0)
+GRAY = (200,200,200)
+RED = (200,0,0)
+GREEN = (0,80,0)
 
 
 # 화면 크기
@@ -44,7 +42,7 @@ def quitGame():
 
 # 텍스트 설정
 def text_objects(text,font):
-    textSurface = font.render(text,True,black)
+    textSurface = font.render(text,True,BLACK)
     return textSurface, textSurface.get_rect()
 
 # 이미지 변환 _카드 크
@@ -86,7 +84,7 @@ def startScreen():
                 #     screen = pygame.display.set_mode((640, 360))
 
 
-        screen.fill(white)
+        screen.fill(WHITE)
 
         # 게임 제목
         largeText = pygame.font.SysFont("malgungothic",115)
@@ -102,7 +100,7 @@ def startScreen():
         buttons = [single_player_button, setting_button, quit_button]
 
         # 방향키로 선택된 버튼 표시
-        pygame.draw.rect(screen,gray,buttons[selected_button_index].rect)
+        pygame.draw.rect(screen,GRAY,buttons[selected_button_index].rect)
         screen.blit(buttons[selected_button_index].textSurf,buttons[selected_button_index].textRect)
         
         pygame.display.update()
@@ -126,14 +124,14 @@ def gameSreen():
                 # 강제 종료
                 if event.key == pygame.K_ESCAPE:
                     # 일시정지 함수 적기
-                    pass
-        
-        screen.fill(green)
+                    pause()
+ 
+        screen.fill(GREEN)
         for i in range(len(playerRect)):
-            pygame.draw.rect(screen,white,playerRect[i],width=0)
+            pygame.draw.rect(screen,WHITE,playerRect[i],width=0)
 
         for i in range(len(playerRect)):
-            sur = font.render("player {}".format(i+1),True,black)
+            sur = font.render("player {}".format(i+1),True,BLACK)
             screen.blit(sur,(playerRect[i][0],playerRect[i][1]))
         
         # 마우스 위치
@@ -185,7 +183,7 @@ def gameSreen():
         # for card in cards:
         #     card.show()
 
-        text = font.render("You",True,black)
+        text = font.render("You",True,BLACK)
         screen.blit(text,playerXY)
     
 
@@ -197,24 +195,60 @@ def settingScreen():
     
     gameExit = False
 
+    # 배경음, 효과음 slider
+    backgroundsound_slider = Slider(screen, 300, (250,180),(0,100))
+    soundeffect_slider = Slider(screen, 300, (250,250),(0,100))
+
+
     while not gameExit:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quitGame()
+            
+            backgroundsound_slider.operate(event)
+            soundeffect_slider.operate(event)
 
-        screen.fill(white)
+        screen.fill(WHITE)
 
-        # 임시, 버튼을 눌렀을 때 화면이 전환되는 지 확인하는 용도
-        largeText = pygame.font.SysFont("malgungothic",115)
+        # 설정화면 텍스트 표시
+        largeText = pygame.font.SysFont("malgungothic",50)
         TextSurf, TextRect = text_objects("설정 화면",largeText)
-        TextRect.center = ((screen_width/2),(screen_height/5))
+        TextRect.center = (130,50)
         screen.blit(TextSurf,TextRect)
+
+        # 화면 설정 텍스트 표시 ==>> 텍스트 표시 함수를 좀 고쳐야함
+        FONT = pygame.font.SysFont("malgungothic",20)
+        text = FONT.render("화면 설정" , True, BLACK)
+        text_rect = text.get_rect()
+        text_rect.center = (130,340)
+        screen.blit(text, text_rect)
+
+        # 화면 설정 버튼 - 변수명 바꿔야함
+        sizefull_button = Button(270, 315, 100, 50, "전체화면",20,color=GRAY, hover_color=YELLOW,action=screenSize)
+        size16_button = Button(420, 315, 100, 50, "16:9",20,color=GRAY, hover_color=YELLOW,action=settingScreen)
+        size4_button = Button(570, 315, 100, 50, "4:3",20,color=GRAY, hover_color=YELLOW,action=quitGame)
+
+
+        # 배경음, 효과음 그림
+        backgroundsound_slider.draw()
+        backgroundsound_slider.text_draw('배경음',(130,180))
+        soundeffect_slider.draw()
+        soundeffect_slider.text_draw('효과음',(130,250))
+
+        # 조작키 설정, 설정 초기화, 설정 저장 버튼 
+        control_button = Button(80, 400, 100, 50, "조작키 설정",20,color=GRAY, hover_color=YELLOW,action=gameSreen)
+        settinginit_button = Button(80, 500, 100, 50, "설정 초기화",20,color=GRAY, hover_color=YELLOW,action=settingScreen)
+        settingsave_button = Button(screen_width // 2 + 200, 500, 100, 50, "설정 저장",20,color=GRAY, hover_color=YELLOW,action=quitGame)
+
+        buttons = [control_button, settinginit_button, settingsave_button]
+
+    
 
         pygame.display.update()
 
 # 버튼 클래스
 class Button:
-    def __init__(self,x,y,width,height,text,font_size=30,font_name='malgungothic', color=white, hover_color=gray,action=None):
+    def __init__(self,x,y,width,height,text,font_size=30,font_name='malgungothic', color=WHITE, hover_color=GRAY,action=None):
         self.rect = pygame.Rect(x,y,width,height) 
         self.text = text
         self.color = color
@@ -263,6 +297,130 @@ class Button:
 #             TextRect.center = (self.x +50, self.y+75)
 #             screen.blit(TextSurf,TextRect)
 
+# 일시정지 함수
+def pause():
+
+    paused = True 
+      
+    selected_button_index = 0
+
+    while paused:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.MOUSEBUTTONDOWN: # 마우스 클릭시 다시 시작 
+                paused = False
+
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN:
+                    if selected_button_index >= 1 :
+                        selected_button_index = 0   
+                    else:
+                        selected_button_index += 1
+                elif event.key == pygame.K_UP:
+                    if selected_button_index <= 0  :
+                        selected_button_index = 1   
+                    else:
+                        selected_button_index -= 1
+                elif event.key == pygame.K_RETURN:
+                    buttons[selected_button_index].action()
+
+        pygame.draw.rect(screen, WHITE ,(screen_width/2-200,screen_height/3-100,400,400))
+        pygame.draw.rect(screen, BLACK ,(screen_width/2-200,screen_height/3-100,400,400),5)
+       
+        
+        setting_button = Button(screen_width // 2 - 100, 300, 200, 50, "Settings",action=settingScreen)
+        quit_button = Button(screen_width // 2 - 100, 400, 200, 50, "Quit",action=quitGame)
+        buttons = [setting_button, quit_button]
+
+    
+        # 방향키로 선택된 버튼 표시
+        pygame.draw.rect(screen,GRAY,buttons[selected_button_index].rect)
+        screen.blit(buttons[selected_button_index].textSurf,buttons[selected_button_index].textRect)
+       
+
+        largeText = pygame.font.SysFont("malgungothic",50)
+        TextSurf, TextRect = text_objects("일시 정지",largeText)
+        TextRect.center = ((screen_width/2),(screen_height/3))
+        screen.blit(TextSurf,TextRect)
+
+        pygame.display.update()
+
+# Slider 클래스 배경음, 효과음 조절
+class Slider: 
+
+    def __init__(self, screen, length, pos, slider_range, main_color = BLACK, button_color = RED):
+        
+        self.drag = False # 버튼을 드래그하기 위한 초기 변수 
+        self.offset_x = 0
+
+        self.screen = screen
+        self.length = length # 슬라이더 크기
+        self.x, self.y = pos # 슬라이더 위치
+        self.min, self.max = slider_range # 슬라이더 값의 범위
+        self.color1 = main_color # 선 색
+        self.color2 = button_color # 버튼색
+
+        button = pygame.rect.Rect(int(self.x + 9 / 20 * self.length), int(self.y - 1 / 20 * self.length), int(self.length * 1 / 10),int(self.length * 1 / 10))      
+        
+        self.value = (self.max + self.min) / 2 # 값의 초기값
+        self.button_rect = button
+
+    def draw(self): # 선, 버튼을 스크린에 그림
+        pygame.draw.line(self.screen,self.color1,(self.x,self.y),(self.x+self.length,self.y),5)
+        pygame.draw.rect(self.screen, self.color2, self.button_rect)
+
+    def operate(self, Event): # pygame 메인 루프에서 event를 받아 드래그 하기 위한 함수
+        if Event.type == pygame.MOUSEBUTTONDOWN:
+            if Event.button == 1:
+                if self.button_rect.collidepoint(Event.pos):
+                    self.drag = True
+
+                    mouse_x, mouse_y = Event.pos
+                    self.offset_x = self.button_rect.x - mouse_x
+
+        elif Event.type == pygame.MOUSEBUTTONUP:
+            if Event.button == 1:
+                self.drag = False
+
+        elif Event.type == pygame.MOUSEMOTION:
+            if self.drag:
+                mouse_x, mouse_y = Event.pos
+                self.button_rect.x = mouse_x + self.offset_x
+                if self.button_rect.x <= self.x  : # 드래그 제한
+                    self.button_rect.x = self.x  
+
+                elif self.button_rect.x >= (self.x + self.length) - 1 / 10 * self.length:
+                    self.button_rect.x = int((self.x + self.length) - 1 / 10 * self.length)
+        
+        self.value = self.max + (self.min - self.max) * (1 - (self.button_rect.x - self.x) / ( 9 / 10 * self.length)) # value 수정
+     
+    # value을 text로 출력 
+    def text_draw(self, name, pos ,color = BLACK , size = 20):
+        FONT = pygame.font.SysFont("malgungothic",size)
+        text = FONT.render("{} : {}".format(name, int(self.value)) , True, color)
+        text_rect = text.get_rect()
+        text_rect.center = pos
+
+        self.screen.blit(text, text_rect)
+
+# 화면 설정 -> 사이즈마다 구현해야 함
+def screenSize():
+    screen_width = 1280
+    screen_height = 800
+    screen = pygame.display.set_mode((screen_width,screen_height))
+ 
+# 조작키 설정
+def controlButtonSetting():
+    pass
+
+# 설정 초기화
+def settingInit():
+    pass
+
+# 설정 저장
+def settingSave():
+    pass
 
 
 
