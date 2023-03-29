edf3d08618f5edf2d58e359eae498cceaaf8581f
Sminp <0102262@gmail.com>
2023-03-29 14:59:04 +0900

    박소민 카드 크기 수정

--- Ingame/constant.py
+++ Ingame/constant.py
@@ -19,8 +19,8 @@ RED = (200, 0, 0)
 GREEN = (0, 80, 0)
 
 # 카드 크기
-CARD_HEIGHT = 585 / 5
-CARD_WIDTH = 410 / 5
+CARD_WIDTH = 60
+CARD_HEIGHT = 80
 CARD_SIZE = (CARD_WIDTH, CARD_HEIGHT)
 
 # # 카드
@@ -34,7 +34,7 @@ CARD_SIZE = (CARD_WIDTH, CARD_HEIGHT)
 # RED_7 = pygame.transform.scale(pygame.image.load("image/card_img/red_7.png"), CARD_SIZE).convert_alpha()
 # RED_8 = pygame.transform.scale(pygame.image.load("image/card_img/red_8.png"), CARD_SIZE).convert_alpha()
 # RED_9 = pygame.transform.scale(pygame.image.load("image/card_img/red_9.png"), CARD_SIZE).convert_alpha()
-# RED_CHANGE = pygame.transform.scale(pygame.image.load("image/card_img/red_change.png"), CARD_SIZE).convert_alpha()
+# RED_CHANGE = pygame.transform.scale(pygame.image.load("image/card_img/red_reverse.png"), CARD_SIZE).convert_alpha()
 # RED_PASS = pygame.transform.scale(pygame.image.load("image/card_img/red_pass.png"), CARD_SIZE).convert_alpha()
 # RED_PLUS_TWO = pygame.transform.scale(pygame.image.load("image/card_img/red_plus_two.png"), CARD_SIZE).convert_alpha()
 #
@@ -48,7 +48,7 @@ CARD_SIZE = (CARD_WIDTH, CARD_HEIGHT)
 # GREEN_7 = pygame.transform.scale(pygame.image.load("image/card_img/green_7.png"), CARD_SIZE).convert_alpha()
 # GREEN_8 = pygame.transform.scale(pygame.image.load("image/card_img/green_8.png"), CARD_SIZE).convert_alpha()
 # GREEN_9 = pygame.transform.scale(pygame.image.load("image/card_img/green_9.png"), CARD_SIZE).convert_alpha()
-# GREEN_CHANGE = pygame.transform.scale(pygame.image.load("image/card_img/green_change.png"), CARD_SIZE).convert_alpha()
+# GREEN_CHANGE = pygame.transform.scale(pygame.image.load("image/card_img/green_reverse.png"), CARD_SIZE).convert_alpha()
 # GREEN_PASS = pygame.transform.scale(pygame.image.load("image/card_img/green_pass.png"), CARD_SIZE).convert_alpha()
 # GREEN_PLUS_TWO = pygame.transform.scale(pygame.image.load("image/card_img/green_plus_two.png"), CARD_SIZE).convert_alpha()
 #
@@ -62,10 +62,10 @@ CARD_SIZE = (CARD_WIDTH, CARD_HEIGHT)
 # BLUE_7 = pygame.transform.scale(pygame.image.load("image/card_img/blue_7.png"), CARD_SIZE).convert_alpha()
 # BLUE_8 = pygame.transform.scale(pygame.image.load("image/card_img/blue_8.png"), CARD_SIZE).convert_alpha()
 # BLUE_9 = pygame.transform.scale(pygame.image.load("image/card_img/blue_9.png"), CARD_SIZE).convert_alpha()
-# BLUE_CHANGE = pygame.transform.scale(pygame.image.load("image/card_img/blue_change.png"), CARD_SIZE).convert_alpha()
+# BLUE_CHANGE = pygame.transform.scale(pygame.image.load("image/card_img/blue_reverse.png"), CARD_SIZE).convert_alpha()
 # BLUE_PASS = pygame.transform.scale(pygame.image.load("image/card_img/blue_pass.png"), CARD_SIZE).convert_alpha()
 # BLUE_PLUS_TWO = pygame.transform.scale(pygame.image.load("image/card_img/blue_plus_two.png"), CARD_SIZE).convert_alpha()
 #
 # BACK = pygame.transform.scale(pygame.image.load("image/card_img/back.png"), CARD_SIZE).convert_alpha()
 # WILD = pygame.transform.scale(pygame.image.load("image/card_img/wild.png"), CARD_SIZE).convert_alpha()
-# WILD_DRAW_FOUR = pygame.transform.scale(pygame.image.load("image/card_img/wild_draw_four.png"), CARD_SIZE).convert_alpha()
+# WILD_DRAW_FOUR = pygame.transform.scale(pygame.image.load("image/card_img/wild_plus_four.png"), CARD_SIZE).convert_alpha()
