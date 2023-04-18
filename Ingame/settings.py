aeed3d98c63f2577191eb48dc0e5d78e3197c559
Sminp <0102262@gmail.com>
2023-04-18 09:54:40 +0900

    박소민 파일 정리

--- Ingame/settings.py
+++ Ingame/settings.py
@@ -6,42 +6,54 @@ import pygame
 
 
 # Rect를 빼고 모두다 좌표로 설정.
+# 버튼 클래스 안에 하이라이트 그림 넣으면 안되려나?
+# 버튼 클래스
+class Button:
+    def __init__(self, screen, x, y, img, width, height):
+        self.screen = screen
+        # self.rect = pygame.Rect(x,y, 30, 30)
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.position = (x, y)
 
-# 이미지 로드 및 크기 변경 함수 (사이즈, 좌표)
-def img_load(screen, filename, size, pos):
-    img = pygame.image.load(filename)
-    img_trans = pygame.transform.scale(img, size)
-    screen.blit(img_trans, pos)
+        self.img = pygame.transform.scale(pygame.image.load(img), [width, height])
+        self.rect = self.img.get_rect()
+        self.rect.center = self.position
+        self.cliked_num = 0
 
+    def show_button(self):
+        self.screen.blit(self.img, (self.x, self.y))
 
-# 텍스트 설정 -> 고치기
-def draw_text(screen, text, pos, size, font="malgungothic"):
-    Font = pygame.font.SysFont(font, size)
-    text1 = Font.render(text,True,BLACK)
-    screen.blit(text1, (pos))
+        # mouse = pygame.mouse.get_pos()
+        # click = pygame.mouse.get_pressed()
 
+        # if x + width > mouse[0] > x and y + height > mouse[1] > y:
+        #     # 마우스를 버튼위에 올렸을 때 색깔 변함
+        #     pygame.draw.rect(screen, self.hover_color, self.rect)
+        #     if click[0] == 1 and action != None:
+        #         # 클릭하면 해당 버튼 기능 수행
+        #         action()
+        # else:
+        #     pygame.draw.rect(screen, self.color, self.rect)
 
-"""설정화면"""
+        # 버튼의 text 설정
+        # font = pygame.font.SysFont(font_name,font_size)
+        # self.textSurf = font.render(text,True,BLACK)
+        # self.textRect = self.textSurf.get_rect()
+        # self.textRect.center = ((x + width/2),(y + height/2))
+        # screen.blit(self.textSurf, self.textRect)
 
+    def cliked(self):
+        self.screen.blit(self.img, (self.x, self.y - 5))
+        self.cliked_num += 1
 
-# 화면 설정 -> 사이즈마다 구현해야 함
-def screen_size(size: int):
-    pass
+    def check_cliked_num(self):
+        pass
 
-# 조작키 설정
-def control_button_setting():
-    pass
-
-
-# 설정 초기화
-def setting_init():
-    pass
-
-
-# 설정 저장
-def setting_save():
-    pass
-
+    def get_rect(self):
+        return self.rect
 
 # Slider 클래스 배경음, 효과음 조절
 class Slider:
@@ -59,18 +71,18 @@ class Slider:
         self.color2 = button_color  # 버튼색
 
         self.button = pygame.rect.Rect(int(self.x + 18 / 40 * self.length), int(self.y - 1 / 40 * self.length),
-                                  int(self.length * 1 / 20), int(self.length * 1 / 20))
+                                       int(self.length * 1 / 20), int(self.length * 1 / 20))
 
         self.value = (self.max + self.min) / 2  # 값의 초기값
         self.button_rect = self.button
-        
+
     def set_value(self, length, pos):
         self.length = length
         self.x, self.y = pos
         self.button.x = int(self.x + 18 / 40 * self.length)
         self.button.y = int(self.y - 1 / 40 * self.length)
-        self.button.width =  int(self.length * 1 / 20)
-        self.button.height =  int(self.length * 1 / 20)
+        self.button.width = int(self.length * 1 / 20)
+        self.button.height = int(self.length * 1 / 20)
 
     def draw(self):  # 선, 버튼을 스크린에 그림
         pygame.draw.line(self.screen, self.color1, (self.x, self.y), (self.x + self.length, self.y), 5)
@@ -110,38 +122,3 @@ class Slider:
         # text_rect.center = pos
 
         self.screen.blit(text1, pos)
-        
-    def get_value(self):
-        return self.value
-
-    # 버튼 클래스
-
-
-# class Button:
-#     def __init__(self, screen, x, y, width, height, text, font_size=30, font_name='malgungothic', color=WHITE,
-#                  hover_color=GRAY, action=None):
-#         self.rect = pygame.Rect(x, y, width, height)
-#         self.text = text
-#         self.color = color
-#         self.hover_color = hover_color
-#         self.action = action
-#         self.screen = screen
-
-#         mouse = pygame.mouse.get_pos()
-#         click = pygame.mouse.get_pressed()
-
-#         if x + width > mouse[0] > x and y + height > mouse[1] > y:
-#             # 마우스를 버튼위에 올렸을 때 색깔 변함
-#             pygame.draw.rect(screen, self.hover_color, self.rect)
-#             if click[0] == 1 and action != None:
-#                 # 클릭하면 해당 버튼 기능 수행
-#                 action()
-#         else:
-#             pygame.draw.rect(screen, self.color, self.rect)
-
-#         # 버튼의 text 설정
-#         font = pygame.font.SysFont(font_name, font_size)
-#         self.text_surf = font.render(self.text, True, BLACK)
-#         self.text_rect = self.text_surf.get_rect()
-#         self.text_rect.center = ((x + width / 2), (y + height / 2))
-#         screen.blit(self.text_surf, self.text_rect)
