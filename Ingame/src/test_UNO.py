d2acaa25c12da75dc104430aac3675a8a0e670c2
Sminp <0102262@gmail.com>
2023-05-15 02:00:52 +0900

    박소민 computer, UNO test 생성

---
+++ Ingame/src/test_UNO.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+
+class TestUNOGame(TestCase):
+    def test_bg_img_load(self):
+        self.fail()
+
+    def test_object_init(self):
+        self.fail()
+
+    def test_object_show(self):
+        self.fail()
+
+    def test_sound(self):
+        self.fail()
+
+    def test_handle_event(self):
+        self.fail()
+
+    def test_menu(self):
+        self.fail()
+
+    def test_key_select_screen(self):
+        self.fail()
