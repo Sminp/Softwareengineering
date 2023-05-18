3f9bb397ee3796827ba2bf29edd3fea0e12e9c2c
Sminp <0102262@gmail.com>
2023-05-18 18:47:24 +0900

    박소민 리팩토링

--- Ingame/rect_functions.py
+++ Ingame/rect_functions.py
@@ -1,14 +1,14 @@
 import pygame
-from constant import *
-from settings import resource_path
+import constant as c
+import settings as s
 
 
-# 오 글씨 바꾸는 기능 있었으면 좋겠다 
+# 오 글씨 바꾸는 기능 있었으면 좋겠다
 
 class TextRect():
     def __init__(self, screen, text, text_size, text_color):
         self.screen = screen
-        self.font = MALGUNGOTHIC
+        self.font = c.MALGUNGOTHIC
         self.text = text
         self.text_size = text_size
         self.text_color = text_color
@@ -20,10 +20,10 @@ class TextRect():
         font = pygame.font.SysFont(self.font, self.text_size)
         surface = font.render(self.text, True, self.text_color)
         return surface
-    
+
     def text_rect(self):
         return self.text_surface().get_rect()
-    
+
     def change_text_surface(self, text):
         font = pygame.font.SysFont(self.font, self.text_size)
         self.surface = font.render(text, True, self.text_color)
@@ -55,9 +55,11 @@ class Button():
         self.position = (x, y)
         self.highlighted = False
 
-        self.img = pygame.transform.scale(pygame.image.load(resource_path(img)), [width, height])
+        self.img = pygame.transform.scale(
+            pygame.image.load(s.resource_path(img)), [width, height])
         self.rect = self.img.get_rect()
-        self.rect.center = (self.x + self.width / 2), (self.y + self.height / 2)
+        self.rect.center = (self.x + self.width /
+                            2), (self.y + self.height / 2)
 
     def show(self):
         self.screen.blit(self.img, (self.x, self.y))
@@ -71,7 +73,7 @@ class Button():
 
 # Slider 클래스 배경음, 효과음 조절
 class Slider():
-    def __init__(self, screen, text, length, pos, slider_range, main_color=BLACK, button_color=RED):
+    def __init__(self, screen, text, length, pos, slider_range, main_color=c.BLACK, button_color=c.RED):
         self.drag = False  # 버튼을 드래그하기 위한 초기 변수
         self.offset_x = 0
         self.screen = screen
@@ -98,7 +100,8 @@ class Slider():
         self.button.height = int(self.length * 1 / 20)
 
     def show(self):  # 선, 버튼을 스크린에 그림
-        pygame.draw.line(self.screen, self.color1, (self.x, self.y), (self.x + self.length, self.y), 5)
+        pygame.draw.line(self.screen, self.color1,
+                         (self.x, self.y), (self.x + self.length, self.y), 5)
         pygame.draw.rect(self.screen, self.color2, self.button_rect)
 
     def operate(self, event):  # pygame 메인 루프에서 event를 받아 드래그 하기 위한 함수
@@ -122,15 +125,17 @@ class Slider():
                     self.button_rect.x = self.x
 
                 elif self.button_rect.x >= (self.x + self.length) - 1 / 10 * self.length:
-                    self.button_rect.x = int((self.x + self.length) - 1 / 10 * self.length)
+                    self.button_rect.x = int(
+                        (self.x + self.length) - 1 / 10 * self.length)
 
         self.value = self.max + (self.min - self.max) * (
-                1 - (self.button_rect.x - self.x) / (9 / 10 * self.length))  # value 수정
+            1 - (self.button_rect.x - self.x) / (9 / 10 * self.length))  # value 수정
 
     # value을 text로 출력
-    def show_value(self, pos, color=BLACK):
+    def show_value(self, pos, color=c.BLACK):
         FONT = pygame.font.SysFont("malgungothic", self.size)
-        text1 = FONT.render("{} : {}".format(self.text, int(self.value)), True, color)
+        text1 = FONT.render("{} : {}".format(
+            self.text, int(self.value)), True, color)
         text_rect = text1.get_rect()
         text_rect.center = pos
 
--- Ingame/settings.py
+++ Ingame/settings.py
@@ -1,6 +1,6 @@
 """게임 기본 설정 저장하는 공간. 버튼 클래스나 setting 클래스가 들어가면 좋지
 시작화면 및 설정화면"""
-from constant import *
+import constant as c
 import sys
 import pygame
 import os
@@ -29,33 +29,32 @@ class Settings:
 
     def init_setting(self):
         # 초기값을 setting.pickle에 저장
-        init_value = {'screen': [800, 600], 'fullscreen': 0, 'font': MALGUNGOTHIC,
+        init_value = {'screen': [800, 600], 'fullscreen': 0, 'font': c.MALGUNGOTHIC,
                       'keys': {
                           "left": pygame.K_LEFT,
                           "right": pygame.K_RIGHT,
                           "up": pygame.K_UP,
                           "down": pygame.K_DOWN,
                           "click": pygame.K_KP_ENTER
-                      },
-                      'sound': {
+        },
+            'sound': {
                           "total": 1,
                           "background": 1,
                           "effect": 1
-                      }
-            , 'setting_color': False,
+        }, 'setting_color': False,
 
-                      'achievement': {'single_win': False, 'storya_win': False, 'storyb_win': False,
-                                      'storyc_win': False, 'storyd_win': False, 'speed_master': False,
-                                      'no_skill_card': False, 'turtle_win': False,
-                                      'first_play': False, 'card_collector': False, 'skill_master': False},
-                      'achievement_date': {'single_win': None, 'storya_win': None, 'storyb_win': None,
-                                           'storyc_win': None, 'storyd_win': None, 'speed_master': None,
-                                           'no_skill_card': None, 'turtle_win': None,
-                                           'first_play': None, 'card_collector': None, 'skill_master': None},
+            'achievement': {'single_win': False, 'storya_win': False, 'storyb_win': False,
+                            'storyc_win': False, 'storyd_win': False, 'speed_master': False,
+                            'no_skill_card': False, 'turtle_win': False,
+                            'first_play': False, 'card_collector': False, 'skill_master': False},
+            'achievement_date': {'single_win': None, 'storya_win': None, 'storyb_win': None,
+                                 'storyc_win': None, 'storyd_win': None, 'speed_master': None,
+                                 'no_skill_card': None, 'turtle_win': None,
+                                 'first_play': None, 'card_collector': None, 'skill_master': None},
 
-                      'win_count': 0  # 승리 횟수
+            'win_count': 0  # 승리 횟수
 
-                      }
+        }
 
         self.set_setting(init_value)
 
@@ -68,13 +67,13 @@ class Settings:
             setting = pickle.load(fi)
 
         return setting
-    
-    def change_setting(self, change_value : dict):
-        self.setting = change_value
-        self.set_setting(self.setting) 
 
+    def change_setting(self, change_value: dict):
+        self.setting = change_value
+        self.set_setting(self.setting)
 
     # 변화 후 값들
+
     def window_change(self, size):
         if size == 16:
             self.screen_width = 1280
@@ -101,4 +100,3 @@ class Settings:
     @screen_height.setter
     def screen_height(self, value):
         self._screen_height = value
-
